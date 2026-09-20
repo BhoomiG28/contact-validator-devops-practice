9277f8eb437dd5c05068b083c61fbb8f07fc9a77
BhoomiG28 <bhoomigarg52@gmail.com>
2026-09-21 00:12:24 +0530

    Add mask email test

--- tests/contact_validator_test.py
+++ tests/contact_validator_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.contact_validator import validate_email
+from src.contact_validator import validate_email, mask_email
 
 
 def test_validate_email_valid():
@@ -13,3 +13,10 @@ def test_validate_email_invalid():
 
 def test_validate_email_no_at():
     assert validate_email("priya.example.com") is False
+
+def test_mask_email_basic():
+    email = "priya@example.com"
+
+    result = mask_email(email)
+
+    assert result == "pr***@example.com"
